f00d567109c8860ff7cccf299eb736ed5d600bdd
yuliia.pavlenko <yuliia.pavlenko@globallgic.com>
2018-06-19 14:41:48 +0300

    added repository

---
+++ src/main/java/com/example/demo/dao/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.dao.repository;
+
+import com.example.demo.dao.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
