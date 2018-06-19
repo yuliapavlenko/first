d21e14d8a9b6d09bfdaca63218608e65b1486a90
yuliia.pavlenko <yuliia.pavlenko@globallgic.com>
2018-06-19 15:30:24 +0300

    added record to data script

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO persons(first_name, last_name) VALUES ('Vasian', 'Petrov');
 INSERT INTO persons(first_name, last_name) VALUES ('Vasiya', 'Ivanov');
+INSERT INTO persons(first_name, last_name) VALUES ('Ivan', 'Ivanov');
