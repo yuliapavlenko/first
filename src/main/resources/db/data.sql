06c95f19b9ad976b8a623da1e48f5842a949db03
yuliia.pavlenko <yuliia.pavlenko@globallgic.com>
2018-06-19 15:33:11 +0300

    deleted record to data script

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,2 @@
 INSERT INTO persons(first_name, last_name) VALUES ('Vasian', 'Petrov');
 INSERT INTO persons(first_name, last_name) VALUES ('Vasiya', 'Ivanov');
-INSERT INTO persons(first_name, last_name) VALUES ('Ivan', 'Ivanov');
