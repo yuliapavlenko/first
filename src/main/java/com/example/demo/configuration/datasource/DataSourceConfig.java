7c6fdbba5cf124c72cf8d2480e9e1ff759f03bbf
yuliia.pavlenko <yuliia.pavlenko@globallgic.com>
2018-06-20 19:52:36 +0300

    added javadoc

--- src/main/java/com/example/demo/configuration/datasource/DataSourceConfig.java
+++ src/main/java/com/example/demo/configuration/datasource/DataSourceConfig.java
@@ -9,6 +9,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+    /** {@inheritDoc} */
     @Bean
     public DataSource dataSource() {
         EmbeddedDatabaseBuilder embeddedDatabaseBuilder = new EmbeddedDatabaseBuilder();
