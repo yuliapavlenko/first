3256a50d5a4840c9703a679d631f99a731c2ff31
yuliia.pavlenko <yuliia.pavlenko@globallgic.com>
2018-06-21 15:24:53 +0300

    added test

---
+++ src/test/java/com/example/demo/repository/PersonRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+public class PersonRepositoryTest {
+	
+	@Test
+	public void whenFindById_thenReturnPerson() {
+		assertEquals("true", "true");
+	}
+}
